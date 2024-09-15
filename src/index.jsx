8f433245a3d03edd03409c9acdacc3872ab29069
kaepy <henna.tuunainen@gmail.com>
2024-09-15 08:01:59 +0300

    Flux & Redux: Redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,52 +1,50 @@
-import ReactDOM from 'react-dom/client'
+// Redux laskuri esimerkki
 
-import { createStore } from 'redux'
+import ReactDOM from "react-dom/client";
+
+// Nykysuositus on käyttää configureStore funktiota createStoren sijasta
+import { createStore } from "redux";
 
 const counterReducer = (state = 0, action) => {
   switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    case 'ZERO':
-      return 0
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    case "ZERO":
+      return 0;
     default:
-      return state
+      return state;
   }
-}
+};
 
-const store = createStore(counterReducer)
+const store = createStore(counterReducer);
 
+// App renderöi laskurin arvon kysymällä sitä storesta metodilla store.getState()
+// Nappien tapahtumankäsittelijät dispatchaavat suoraan oikean tyyppiset actionit storelle.
 const App = () => {
   return (
     <div>
-      <div>
-        {store.getState()}
-      </div>
-      <button 
-        onClick={() => store.dispatch({ type: 'INCREMENT' })}
-      >
+      <div>{store.getState()}</div>
+      <button onClick={() => store.dispatch({ type: "INCREMENT" })}>
         plus
       </button>
-      <button
-        onClick={() => store.dispatch({ type: 'DECREMENT' })}
-      >
+      <button onClick={() => store.dispatch({ type: "DECREMENT" })}>
         minus
       </button>
-      <button 
-        onClick={() => store.dispatch({ type: 'ZERO' })}
-      >
-        zero
-      </button>
+      <button onClick={() => store.dispatch({ type: "ZERO" })}>zero</button>
     </div>
-  )
-}
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// Koko sovelluksen renderöinnin suorittava funktio
 const renderApp = () => {
-  root.render(<App />)
-}
+  root.render(<App />);
+};
 
-renderApp()
-store.subscribe(renderApp)
+// Suorittaa sovelluksen ensimmäisen renderöinnin, ilman tätä sitä ei tapahtuisi ollenkaan
+renderApp();
+// renderApp kuuntelee storen muutoksia metodilla subscribe
+store.subscribe(renderApp);
