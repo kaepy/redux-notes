72ec8bd95d67315f3b75802d59d2f314e2dc77b0
kaepy <henna.tuunainen@gmail.com>
2024-09-18 20:06:45 +0300

    Flux & Redux: Lisää komponentteja

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
-import { createNote, toggleImportanceOf } from "./reducers/noteReducer";
-import { useSelector, useDispatch } from "react-redux";
+import NewNote from "./components/NewNote";
+import Notes from "./components/Notes";
 
 const App = () => {
-  const dispatch = useDispatch();
-  const notes = useSelector((state) => state); // sama kuin (state) => { return state } eli hakee kaikki muistiinpanot
-  // Hakisi vain tärkeät muistiinpanot: const importantNotes = useSelector(state => state.filter(note => note.important))
-
-  // muistiinpanon lisäyksen käsittelevä metodi dispatchaa muistiinpanon lisäävän actionin
-  const addNote = (event) => {
-    event.preventDefault();
-    const content = event.target.note.value;
-    event.target.note.value = "";
-    dispatch(createNote(content));
-  };
-
-  const toggleImportance = (id) => {
-    dispatch(toggleImportanceOf(id));
-  };
-
   return (
     <div>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
-      </form>
-      <ul>
-        {notes.map((note) => (
-          <li key={note.id} onClick={() => toggleImportance(note.id)}>
-            {note.content}
-            <strong>{note.important ? " important" : ""}</strong>
-          </li>
-        ))}
-      </ul>
+      <NewNote />
+      <Notes />
     </div>
   );
 };
